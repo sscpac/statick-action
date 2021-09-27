5722df8a4acea4102d5567c7b36d9a5bd2badb1a
Greg Kogut <kogut@spawar.navy.mil>
2021-09-27 11:00:51 -0700

    Adding default value for statick 'check'

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,6 +15,9 @@ if [ -n "$INPUT_CHECK" ]; then
   if [ "$INPUT_CHECK" = "true" ]; then
     STATICK_ARGS+=" --check"
   fi
+# default to true if no value provided
+else
+  STATICK_ARGS+=" --check"
 fi
 
 if [ -n "$INPUT_CONFIG" ]; then
